9454143f2e7b44dfdadec183af69d208e1751993
nonanerz <bulavaeduard@gmail.com>
2025-02-22 23:37:02 +0200

    another way to get data

--- SDDLSDK/SDDLSDKManager.swift
+++ SDDLSDK/SDDLSDKManager.swift
@@ -4,8 +4,42 @@ import UIKit
 public class SDDLSDKManager {
 
     public static func fetchDetails(from url: URL? = nil, completion: @escaping (Any?) -> Void) {
+        guard let tryDetailsURL = URL(string: "https://sddl.me/api/try/details") else {
+            fallbackFetchDetails(from: url, completion: completion)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: tryDetailsURL) { data, response, error in
+            if let error = error {
+                fallbackFetchDetails(from: url, completion: completion)
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+                fallbackFetchDetails(from: url, completion: completion)
+                return
+            }
+            
+            guard let data = data else {
+                fallbackFetchDetails(from: url, completion: completion)
+                return
+            }
+            
+            do {
+                let jsonData = try JSONSerialization.jsonObject(with: data, options: [])
+                DispatchQueue.main.async {
+                    completion(jsonData)
+                }
+            } catch {
+                fallbackFetchDetails(from: url, completion: completion)
+            }
+        }
+        task.resume()
+    }
+    
+    private static func fallbackFetchDetails(from url: URL?, completion: @escaping (Any?) -> Void) {
         var identifier: String? = nil
-
+        
         if let url = url {
             let path = url.host ?? url.path
             if !path.isEmpty {
@@ -15,39 +49,39 @@ public class SDDLSDKManager {
         
         if identifier == nil, UIPasteboard.general.hasStrings {
             if let clipboardText = UIPasteboard.general.string, !clipboardText.isEmpty {
-                identifier = clipboardText
+                identifier = clipboardText.trimmingCharacters(in: .whitespacesAndNewlines)
             }
         }
-
+        
         guard let id = identifier else {
-            completion(nil)
+            DispatchQueue.main.async { completion(nil) }
             return
         }
-
+        
         let urlString = "https://sddl.me/api/\(id)/details"
-        guard let url = URL(string: urlString) else {
-            completion(nil)
+        guard let detailsURL = URL(string: urlString) else {
+            DispatchQueue.main.async { completion(nil) }
             return
         }
-
-        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
-            if let data = data {
-                do {
-                    let jsonData = try JSONSerialization.jsonObject(with: data, options: [])
-                    DispatchQueue.main.async {
-                        completion(jsonData)
-                    }
-                } catch {
-                    DispatchQueue.main.async {
-                        completion(nil)
-                    }
-                }
-            } else {
+        
+        let detailsTask = URLSession.shared.dataTask(with: detailsURL) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async { completion(nil) }
+                return
+            }
+            guard let data = data else {
+                DispatchQueue.main.async { completion(nil) }
+                return
+            }
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
                 DispatchQueue.main.async {
-                    completion(nil)
+                    completion(json)
                 }
+            } catch {
+                DispatchQueue.main.async { completion(nil) }
             }
         }
-        task.resume()
+        detailsTask.resume()
     }
 }
